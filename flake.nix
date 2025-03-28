ca9a89aad4609c26f33f87225d41690a2d7cdcff
Ari Gato <ari@is-a.cat>
2025-03-28 10:16:58 +0100

    chore: hack

--- flake.nix
+++ flake.nix
@@ -12,9 +12,9 @@
       lib = nixpkgs.lib;
       supportedSystems = [
         "x86_64-linux"
-        "i686-linux"
-        "aarch64-linux"
-        "riscv64-linux"
+        # "i686-linux"
+        # "aarch64-linux"
+        # "riscv64-linux"
       ];
       forAllSystems = lib.genAttrs supportedSystems;
 
